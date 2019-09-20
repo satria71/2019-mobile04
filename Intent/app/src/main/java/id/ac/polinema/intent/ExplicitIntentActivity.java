4a660187680c9c2388b71f8dd5d65f72083bd7bf
Satria Putra Sabana <satriabumiputralangit@gmail.com>
2019-09-20 15:36:09 +0700

    EditText to TextView

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -2,17 +2,36 @@ package id.ac.polinema.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
+    EditText name;
+    TextView output;
+    private final String KEY_NAME = "name";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        name = findViewById(R.id.input_name);
+        output = findViewById(R.id.text_output);
     }
+
+
+    public void handleButton(View view){
+        String nama = name.getText().toString();
+//        if(nama.equalsIgnoreCase()){
+//            Toast()
+//        }
+        output.setText("Hello "+nama+" Congratulation");
+
+    }
+
+
 
 }
