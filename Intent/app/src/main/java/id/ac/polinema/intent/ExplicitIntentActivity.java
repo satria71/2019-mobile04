f87e17f6ec8eefa65e21f99fffd14510be9dd5d7
dinitriana99 <dinitd25@gmail.com>
2019-09-22 21:33:07 +0700

    Explicit Intent

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -15,4 +15,9 @@ public class ExplicitIntentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_explicit_intent);
     }
 
+    public void handleShow(View view) {
+        EditText teksedit = (EditText) findViewById(R.id.input_name);
+        TextView text = (TextView) findViewById(R.id.text_output);
+        text.setText("Hello "+teksedit.getText()+", Congratulations!");
+    }
 }
