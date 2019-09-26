bcb78bc64f7952d36ab94cf2f892fb37c1974da2
Satria Putra Sabana <satriabumiputralangit@gmail.com>
2019-09-26 21:21:16 +0700

    Mengambil data dan menampilkannya menggunakan parcelabels

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
@@ -3,8 +3,12 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import id.ac.polinema.intent.model.User;
 
 public class ProfileParcelableActivity extends AppCompatActivity {
+    private TextView usernameText, nameText, ageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,11 +16,18 @@ public class ProfileParcelableActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_parcelable);
 
         // TODO: bind here
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
 
             // TODO: display value here
+            User user = extras.getParcelable(ParcelableActivity.USER_KEY);
+            usernameText.setText(user.getUsername());
+            nameText.setText(user.getName());
+            ageText.setText(String.valueOf(user.getAge()));
         }
     }
 }
